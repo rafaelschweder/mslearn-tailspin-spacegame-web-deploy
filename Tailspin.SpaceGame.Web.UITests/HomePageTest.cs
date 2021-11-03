81f553890f40ff62f760f56bc047c3eed59f6ca5
Rafael Schweder <rafaelschweder@hotmail.com>
2021-11-02 22:17:03 -0300

    Run Selenium UI tests

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -92,7 +92,7 @@ namespace UITests
         [TestCase("download-btn", "pretend-modal")]
         // Screen image
         [TestCase("screen-01", "screen-modal")]
-        // // Top player on the leaderboard
+        // Top player on the leaderboard
         [TestCase("profile-1", "profile-modal-1")]
         public void ClickLinkById_ShouldDisplayModalById(string linkId, string modalId)
         {
